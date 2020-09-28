9cc725bb04d77529e82f7899ce34a45c540486fa
Aaron Shames <aaron@1pbx.io>
2020-09-28 17:37:33 +0000

    notify admit to config for multi server

--- setup.sh
+++ setup.sh
@@ -25,3 +25,15 @@ cp system/systemd/kazoo-couchdb.service /usr/lib/systemd/system/
 
 
 systemctl enable kazoo-couchdb
+
+echo "You may now start couchdb with systemctl start kazoo-couchdb"
+echo ""
+echo "Default configuration is for 1 couchdb instance you should edit before starting for a cluster"
+echo "Open config file with Nano?"
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+nano /etc/kazoo/couchdb/local.ini
